11b77e02a7ba342725c7c27d745c0930a0f92c93
ARBUCHELI <radio_seattle@hotmail.com>
2024-10-18 20:51:56 +0300

    base case code

---
+++ 4. ALGORITHMIC CONCEPTS/RECURSION/RECURSIVE VS ITERATIVE TRAVERSAL/base-case/index.js
@@ -0,0 +1,17 @@
+const Node = require('./Node');
+const LinkedList = require('./LinkedList');
+
+const myList = new LinkedList();
+
+myList.addToHead('Node 1');
+myList.addToHead('Node 2');
+myList.addToHead('Node 3');
+myList.addToHead('Node 4');
+
+// Add checkpoint 3 code below:
+const myNodeRecursive = myList.findNodeRecursively('Node 4');
+console.log(myNodeRecursive);
+
+
+
+
